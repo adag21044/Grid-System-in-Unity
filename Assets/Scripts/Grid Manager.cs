a13f89828c331a11b25e6f02f18cc3eaa52267a9
Ahmet E. Dag <adag21044@gmail.com>
2024-09-01 11:45:16 +0300

    Done

--- Assets/Scripts/Grid Manager.cs
+++ Assets/Scripts/Grid Manager.cs
@@ -7,14 +7,22 @@ public class GridManager : MonoBehaviour
     [SerializeField] private int _width, _height;
     [SerializeField] private Tile _tilePrefab;
     [SerializeField] private Transform _cam;
+    private Dictionary<Vector2, Tile> _tiles;
 
     void Start()
     {
         GenerateGrid();
     }
 
+    void Update()
+    {
+        DetectTileClick();
+    }
+
     void GenerateGrid()
     {
+        _tiles = new Dictionary<Vector2, Tile>();
+
         for (int x = 0; x < _width; x++)
         {
             for (int y = 0; y < _height; y++)
@@ -24,9 +32,37 @@ public class GridManager : MonoBehaviour
 
                 var isOffset = ((x % 2 == 0) && (y % 2 == 0)) || ((x % 2 != 0) && (y % 2 != 0));
                 SpawnedTile.Init(isOffset);
+
+                _tiles[new Vector2(x, y)] = SpawnedTile;
+
             }
         }
 
         _cam.transform.position = new Vector3((float)_width / 2 - 0.5f, (float)_height / 2 - 0.5f, -10);
+    }
+
+    void DetectTileClick()
+    {
+        if (Input.GetMouseButtonDown(0)) // Left-click detection
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 gridPosition = new Vector2(Mathf.RoundToInt(mousePosition.x), Mathf.RoundToInt(mousePosition.y));
+
+            Tile clickedTile = GetTileAtPosition(gridPosition);
+            if (clickedTile != null)
+            {
+                Debug.Log($"Clicked on: {clickedTile.name} at position {gridPosition}");
+            }
+            else
+            {
+                Debug.Log("No tile found at this position.");
+            }
+        }
+    }
+
+    public Tile GetTileAtPosition(Vector2 pos)
+    {
+        if(_tiles.TryGetValue(pos, out var tile)) return tile;
+        else return null;
     }
 }
